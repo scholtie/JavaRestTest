084011bc27e201b236553b6efa96dc8c802061dd
Daniel Solti <solti.dani7@gmail.com>
2022-06-21 17:14:13 +0200

    init commit (spring with dependencies)
    Dependencies: PostreSQL driver, Lombok, Spring Data JPA
    -Gradle for logging

---
+++ src/main/java/com/solti/javaendpointtest/JavaEndpointTestApplication.java
@@ -0,0 +1,13 @@
+package com.solti.javaendpointtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEndpointTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEndpointTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/solti/javaendpointtest/JavaEndpointTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solti.javaendpointtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEndpointTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
